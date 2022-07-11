5c5fbb10af9eb07002416807f74e195e18062c5d
evgeniya <evgeniya2689@yandex.ru>
2022-07-11 12:06:48 +0500

    Add tasks 41 and 43

---
+++ Task_41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество чисел, которое будет вводиться с клавиатуры ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine($"Введите {i + 1} элемент");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int CountPositiveInt(int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) result++;
+    }
+    return result;
+}
+int count = CountPositiveInt(array);
+
+Console.WriteLine($"Количество положительных чисел равно {count}");
+
+
+
---
+++ Task_43/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+Console.WriteLine("Введите значение b1 ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k1 ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2 ");
+double num3 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2 ");
+double num4 = Convert.ToDouble(Console.ReadLine());
+
+double XPoint(double number1, double number2, double number3, double number4)
+{
+    double result = (num3 - num1) / (num2 - num4);
+    return result;
+}
+double x = XPoint(num1, num2, num3, num4);
+
+double YPoint(double n1, double n2, double n3, double n4)
+{
+    double result = n2 * ((n3 - n1) / (n2 - n4)) + n1;
+    return result;
+}
+double y = YPoint(num1, num2, num3, num4);
+
+if (num2 == num4)
+    Console.WriteLine("Прямые параллельны ");
+else 
+    Console.WriteLine($"Точка x равна {x}, точка y равна {y} ");
+
